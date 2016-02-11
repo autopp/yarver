820531d2b376f711c76134febdd185d31699df30
autopp <autopp.inc@gmail.com>
2016-02-11 11:08:31 +0900

    fix rubocop warning and test

--- lib/yarver.rb
+++ lib/yarver.rb
@@ -1,5 +1,9 @@
-require "yarver/version"
+require 'yarver/version'
 
+# Module Yarver provides previewer YARV's bytecode
+#
+# @author autopp <autopp.inc@gmail.com>
+#
 module Yarver
   # Your code goes here...
 end
